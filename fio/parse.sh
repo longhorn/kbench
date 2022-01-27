39bf5cadc06fe81571a31f0a9dc2449870392bd1
Derek Su <derek.su@suse.com>
2022-01-27 05:13:14 +0000

    Make CPU idleness profiling as an option
    The CPU idleness introduces extra overhead and storage performance drop.
    By default, the profiling is disabled.
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- fio/parse.sh
+++ fio/parse.sh
@@ -36,51 +36,75 @@ fi
 
 RESULT=${1}.summary
 
-QUICK_MODE_TEXT="QUICK MODE: DISABLED"
+QUICK_MODE_TEXT="Quick Mode: disabled"
 if [ -n "$QUICK_MODE" ]; then
-	QUICK_MODE_TEXT="QUICK MODE ENABLED"
+	QUICK_MODE_TEXT="Quick Mode: enabled"
 fi
 
-SIZE_TEXT="SIZE: 10g"
+SIZE_TEXT="Size: 10g"
 if [ -n "$SIZE" ]; then
-	SIZE_TEXT="SIZE: $SIZE"
+	SIZE_TEXT="Size: $SIZE"
 fi
 
 SUMMARY="
-=====================
+=========================
 FIO Benchmark Summary
 For: $PREFIX
+CPU Idleness Profiling: $CPU_IDLE_PROF
 $SIZE_TEXT
 $QUICK_MODE_TEXT
-=====================
+=========================
 "
 
-printf -v cxt "IOPS (Read/Write)\n$FMT$FMT$FMT\n"\
-	"Random:" \
-	"$(commaize $RAND_READ_IOPS) / $(commaize $RAND_WRITE_IOPS)" \
-	"Sequential:" \
-	"$(commaize $SEQ_READ_IOPS) / $(commaize $SEQ_WRITE_IOPS)" \
-	"CPU Idleness:" \
-	"$CPU_IDLE_PCT_IOPS%"
-SUMMARY+=$cxt
+if [ x"$CPU_IDLE_PROF" = x"enabled" ]; then
+	printf -v cxt "IOPS (Read/Write)\n$FMT$FMT$FMT\n"\
+		"Random:" \
+		"$(commaize $RAND_READ_IOPS) / $(commaize $RAND_WRITE_IOPS)" \
+		"Sequential:" \
+		"$(commaize $SEQ_READ_IOPS) / $(commaize $SEQ_WRITE_IOPS)" \
+		"CPU Idleness:" \
+		"$CPU_IDLE_PCT_IOPS%"
+	SUMMARY+=$cxt
 
-printf -v cxt "Bandwidth in KiB/sec (Read/Write)\n$FMT$FMT$FMT\n"\
-	"Random:" \
-	"$(commaize $RAND_READ_BW) / $(commaize $RAND_WRITE_BW)" \
-	"Sequential:" \
-	"$(commaize $SEQ_READ_BW) / $(commaize $SEQ_WRITE_BW)" \
-	"CPU Idleness:" \
-	"$CPU_IDLE_PCT_BW%"
-SUMMARY+=$cxt
+	printf -v cxt "Bandwidth in KiB/sec (Read/Write)\n$FMT$FMT$FMT\n"\
+		"Random:" \
+		"$(commaize $RAND_READ_BW) / $(commaize $RAND_WRITE_BW)" \
+		"Sequential:" \
+		"$(commaize $SEQ_READ_BW) / $(commaize $SEQ_WRITE_BW)" \
+		"CPU Idleness:" \
+		"$CPU_IDLE_PCT_BW%"
+	SUMMARY+=$cxt
 
-printf -v cxt "Latency in ns (Read/Write)\n$FMT$FMT$FMT\n"\
-	"Random:" \
-	"$(commaize $RAND_READ_LAT) / $(commaize $RAND_WRITE_LAT)" \
-	"Sequential:" \
-	"$(commaize $SEQ_READ_LAT) / $(commaize $SEQ_WRITE_LAT)" \
-	"CPU Idleness:" \
-	"$CPU_IDLE_PCT_LAT%"
-SUMMARY+=$cxt
+	printf -v cxt "Latency in ns (Read/Write)\n$FMT$FMT\n"\
+		"Random:" \
+		"$(commaize $RAND_READ_LAT) / $(commaize $RAND_WRITE_LAT)" \
+		"Sequential:" \
+		"$(commaize $SEQ_READ_LAT) / $(commaize $SEQ_WRITE_LAT)" \
+		"CPU Idleness:" \
+		"$CPU_IDLE_PCT_LAT%"
+	SUMMARY+=$cxt
+else
+	printf -v cxt "IOPS (Read/Write)\n$FMT$FMT\n"\
+		"Random:" \
+		"$(commaize $RAND_READ_IOPS) / $(commaize $RAND_WRITE_IOPS)" \
+		"Sequential:" \
+		"$(commaize $SEQ_READ_IOPS) / $(commaize $SEQ_WRITE_IOPS)"
+	SUMMARY+=$cxt
+
+	printf -v cxt "Bandwidth in KiB/sec (Read/Write)\n$FMT$FMT$FMT\n"\
+		"Random:" \
+		"$(commaize $RAND_READ_BW) / $(commaize $RAND_WRITE_BW)" \
+		"Sequential:" \
+		"$(commaize $SEQ_READ_BW) / $(commaize $SEQ_WRITE_BW)"
+	SUMMARY+=$cxt
+
+	printf -v cxt "Latency in ns (Read/Write)\n$FMT$FMT\n"\
+		"Random:" \
+		"$(commaize $RAND_READ_LAT) / $(commaize $RAND_WRITE_LAT)" \
+		"Sequential:" \
+		"$(commaize $SEQ_READ_LAT) / $(commaize $SEQ_WRITE_LAT)"
+	SUMMARY+=$cxt
+fi
 
 echo "$SUMMARY" > $RESULT
 cat $RESULT
