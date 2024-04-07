eaf910fb7af697782aa237a4c0ff134937f8f71c
Derek Su <derek.su@suse.com>
2024-04-07 17:22:55 +0000

    Customizable IO testing with selective IO types and metrics
    Users have the option to select specific IO types, including random read (randread),
    random write (randwrite), sequential read (seqread), and sequential write (seqwrite),
    as well as metrics like IOPS, bandwidth, and latency.
    There's no requirement to conduct all tests.
    
    Longhorn 8254
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- fio/cmp_parse.sh
+++ fio/cmp_parse.sh
@@ -5,82 +5,91 @@ set -e
 CURRENT_DIR="$(dirname "$(readlink -f "$0")")"
 source $CURRENT_DIR/func.sh
 
+if [ -z "${1}" ]; then
+    echo Require FIO IO types
+    exit 1
+fi
+IO_TYPES="${1}"
+
+if [ -z "${2}" ]; then
+    echo Require FIO metrics
+    exit 1
+fi
+METRICS="${2}"
+
 if [ -z "FIRST_VOL_NAME" ]; then
-	echo Require the first volume name
-	exit 1
+    echo Require the first volume name
+    exit 1
 fi
 
 if [ -z "SECOND_VOL_NAME" ]; then
-	echo Require the second volume name
-	exit 1
+    echo Require the second volume name
+    exit 1
 fi
 
-FIRST_IOPS=${FIRST_VOL_NAME}-iops.json
-FIRST_BW=${FIRST_VOL_NAME}-bandwidth.json
-FIRST_LAT=${FIRST_VOL_NAME}-latency.json
+IFS=',' read -r -a io_types_array <<< "${IO_TYPES}"
+IFS=',' read -r -a metrics_array <<< "${METRICS}"
 
-SECOND_IOPS=${SECOND_VOL_NAME}-iops.json
-SECOND_BW=${SECOND_VOL_NAME}-bandwidth.json
-SECOND_LAT=${SECOND_VOL_NAME}-latency.json
+to_uppercase() {
+    local input_str="$1"
+    local uppercase_str="${input_str^^}" # Convert to uppercase
+    echo "$uppercase_str"
+}
 
-parse_iops $FIRST_IOPS
-FIRST_RAND_READ_IOPS=$RAND_READ_IOPS
-FIRST_RAND_WRITE_IOPS=$RAND_WRITE_IOPS
-FIRST_SEQ_READ_IOPS=$SEQ_READ_IOPS
-FIRST_SEQ_WRITE_IOPS=$SEQ_WRITE_IOPS
-FIRST_CPU_IDLE_PCT_IOPS=$CPU_IDLE_PCT_IOPS
+parse_metrics() {
+    local vol_name="$1"
+    local prefix="$2"
 
-parse_iops $SECOND_IOPS
-SECOND_RAND_READ_IOPS=$RAND_READ_IOPS
-SECOND_RAND_WRITE_IOPS=$RAND_WRITE_IOPS
-SECOND_SEQ_READ_IOPS=$SEQ_READ_IOPS
-SECOND_SEQ_WRITE_IOPS=$SEQ_WRITE_IOPS
-SECOND_CPU_IDLE_PCT_IOPS=$CPU_IDLE_PCT_IOPS
+    if [[ -z "$vol_name" || -z "$prefix" ]]; then
+        echo "parse_metrics: Missing required parameters." >&2
+        return 1
+    fi
+
+    for METRIC in "${metrics_array[@]}"; do
+        for IO_TYPE in "${io_types_array[@]}"; do
+            local output="${vol_name}-${IO_TYPE}-${METRIC}.json"
+            local parse_func="parse_${IO_TYPE}_${METRIC}"
+
+            if declare -f "$parse_func" > /dev/null; then
+                $parse_func "$output"
+
+                local metric=`to_uppercase $METRIC`
+                local io_type=`to_uppercase $IO_TYPE`
+                local var_suffix=""
+                local var_name=""
+
+                var_suffix="${io_type}_${metric^^}"
+                var_name="${prefix}_${var_suffix}"
+                declare -g "$var_name=${!var_suffix}"
+
+                var_suffix="CPU_IDLE_PCT_${io_type}_${metric^^}"
+                var_name="${prefix}_${var_suffix}"
+                declare -g "$var_name=${!var_suffix}"
+            else
+                echo "parse_metrics: Parser function '$parse_func' not found for $output." >&2
+            fi
+        done
+    done
+}
+
+parse_metrics "$FIRST_VOL_NAME" "FIRST"
+parse_metrics "$SECOND_VOL_NAME" "SECOND"
 
 calc_cmp_iops
+calc_cmp_bandwidth
+calc_cmp_latency
 
-parse_bw $FIRST_BW
-FIRST_RAND_READ_BW=$RAND_READ_BW
-FIRST_RAND_WRITE_BW=$RAND_WRITE_BW
-FIRST_SEQ_READ_BW=$SEQ_READ_BW
-FIRST_SEQ_WRITE_BW=$SEQ_WRITE_BW
-FIRST_CPU_IDLE_PCT_BW=$CPU_IDLE_PCT_BW
-
-parse_bw $SECOND_BW
-SECOND_RAND_READ_BW=$RAND_READ_BW
-SECOND_RAND_WRITE_BW=$RAND_WRITE_BW
-SECOND_SEQ_READ_BW=$SEQ_READ_BW
-SECOND_SEQ_WRITE_BW=$SEQ_WRITE_BW
-SECOND_CPU_IDLE_PCT_BW=$CPU_IDLE_PCT_BW
-
-calc_cmp_bw
-
-parse_lat $FIRST_LAT
-FIRST_RAND_READ_LAT=$RAND_READ_LAT
-FIRST_RAND_WRITE_LAT=$RAND_WRITE_LAT
-FIRST_SEQ_READ_LAT=$SEQ_READ_LAT
-FIRST_SEQ_WRITE_LAT=$SEQ_WRITE_LAT
-FIRST_CPU_IDLE_PCT_LAT=$CPU_IDLE_PCT_LAT
-
-parse_lat $SECOND_LAT
-SECOND_RAND_READ_LAT=$RAND_READ_LAT
-SECOND_RAND_WRITE_LAT=$RAND_WRITE_LAT
-SECOND_SEQ_READ_LAT=$SEQ_READ_LAT
-SECOND_SEQ_WRITE_LAT=$SEQ_WRITE_LAT
-SECOND_CPU_IDLE_PCT_LAT=$CPU_IDLE_PCT_LAT
-
-calc_cmp_lat
-
+# Build the summary with header information
 RESULT=${FIRST_VOL_NAME}_vs_${SECOND_VOL_NAME}.summary
 
 QUICK_MODE_TEXT="Quick Mode: disabled"
 if [ -n "$QUICK_MODE" ]; then
-	QUICK_MODE_TEXT="Quick Mode: enabled"
+    QUICK_MODE_TEXT="Quick Mode: enabled"
 fi
 
 SIZE_TEXT="SIZE: 10g"
 if [ -n "$SIZE" ]; then
-	SIZE_TEXT="Size: $SIZE"
+    SIZE_TEXT="Size: $SIZE"
 fi
 
 SUMMARY="
@@ -94,118 +103,121 @@ $QUICK_MODE_TEXT
 "
 
 printf -v header "$CMP_FMT" \
-	"" $FIRST_VOL_NAME "vs" $SECOND_VOL_NAME ":" "Change"
+    "" $FIRST_VOL_NAME "vs" $SECOND_VOL_NAME ":" "Change"
 SUMMARY+=$header
 
-if [ x"$CPU_IDLE_PROF" = x"enabled" ]; then
-	printf -v cxt "IOPS (Read/Write)\n$CMP_FMT$CMP_FMT$CMP_FMT\n" \
-		"Random:" \
-		"$(commaize $FIRST_RAND_READ_IOPS) / $(commaize $FIRST_RAND_WRITE_IOPS)" \
-		"vs" \
-		"$(commaize $SECOND_RAND_READ_IOPS) / $(commaize $SECOND_RAND_WRITE_IOPS)" \
-		":" \
-		"$CMP_RAND_READ_IOPS / $CMP_RAND_WRITE_IOPS" \
-		"Sequential:" \
-		"$(commaize $FIRST_SEQ_READ_IOPS) / $(commaize $FIRST_SEQ_WRITE_IOPS)" \
-		"vs" \
-		"$(commaize $SECOND_SEQ_READ_IOPS) / $(commaize $SECOND_SEQ_WRITE_IOPS)" \
-		":" \
-		"$CMP_SEQ_READ_IOPS / $CMP_SEQ_WRITE_IOPS" \
-		"CPU Idleness:" \
-		"$FIRST_CPU_IDLE_PCT_IOPS%" \
-		"vs" \
-		"$SECOND_CPU_IDLE_PCT_IOPS%"\
-		":" \
-		"$CMP_CPU_IDLE_PCT_IOPS%"
-	SUMMARY+=$cxt
+#!/bin/bash
 
-	printf -v cxt "Bandwidth in KiB/sec (Read/Write)\n$CMP_FMT$CMP_FMT$CMP_FMT\n" \
-		"Random:" \
-		"$(commaize $FIRST_RAND_READ_BW) / $(commaize $FIRST_RAND_WRITE_BW)" \
-		"vs" \
-		"$(commaize $SECOND_RAND_READ_BW) / $(commaize $SECOND_RAND_WRITE_BW)" \
-		":" \
-		"$CMP_RAND_READ_BW / $CMP_RAND_WRITE_BW" \
-		"Sequential:" \
-		"$(commaize $FIRST_SEQ_READ_BW) / $(commaize $FIRST_SEQ_WRITE_BW)" \
-		"vs" \
-		"$(commaize $SECOND_SEQ_READ_BW) / $(commaize $SECOND_SEQ_WRITE_BW)" \
-		":" \
-		"$CMP_SEQ_READ_BW / $CMP_SEQ_WRITE_BW" \
-		"CPU Idleness:" \
-		"$FIRST_CPU_IDLE_PCT_BW%" \
-		"vs" \
-		"$SECOND_CPU_IDLE_PCT_BW%" \
-		":" \
-		"$CMP_CPU_IDLE_PCT_BW%"
-	SUMMARY+=$cxt
+# Define a function to add metrics to the summary
+add_metrics_to_summary() {
+    local metric_name="${1}"
+    local metric_unit="${2}"
 
-	printf -v cxt "Latency in ns (Read/Write)\n$CMP_FMT$CMP_FMT$CMP_FMT\n" \
-		"Random:" \
-		"$(commaize $FIRST_RAND_READ_LAT) / $(commaize $FIRST_RAND_WRITE_LAT)" \
-		"vs" \
-		"$(commaize $SECOND_RAND_READ_LAT) / $(commaize $SECOND_RAND_WRITE_LAT)" \
-		":" \
-		"$CMP_RAND_READ_LAT / $CMP_RAND_WRITE_LAT" \
-		"Sequential:" \
-		"$(commaize $FIRST_SEQ_READ_LAT) / $(commaize $FIRST_SEQ_WRITE_LAT)" \
-		"vs" \
-		"$(commaize $SECOND_SEQ_READ_LAT) / $(commaize $SECOND_SEQ_WRITE_LAT)" \
-		":" \
-		"$CMP_SEQ_READ_LAT / $CMP_SEQ_WRITE_LAT" \
-		"CPU Idleness:" \
-		"$FIRST_CPU_IDLE_PCT_LAT%" \
-		"vs" \
-		"$SECOND_CPU_IDLE_PCT_LAT%" \
-		":" \
-		"$CMP_CPU_IDLE_PCT_LAT%"
-		SUMMARY+=$cxt
-else
-	printf -v cxt "IOPS (Read/Write)\n$CMP_FMT$CMP_FMT\n" \
-		"Random:" \
-		"$(commaize $FIRST_RAND_READ_IOPS) / $(commaize $FIRST_RAND_WRITE_IOPS)" \
-		"vs" \
-		"$(commaize $SECOND_RAND_READ_IOPS) / $(commaize $SECOND_RAND_WRITE_IOPS)" \
-		":" \
-		"$CMP_RAND_READ_IOPS / $CMP_RAND_WRITE_IOPS" \
-		"Sequential:" \
-		"$(commaize $FIRST_SEQ_READ_IOPS) / $(commaize $FIRST_SEQ_WRITE_IOPS)" \
-		"vs" \
-		"$(commaize $SECOND_SEQ_READ_IOPS) / $(commaize $SECOND_SEQ_WRITE_IOPS)" \
-		":" \
-		"$CMP_SEQ_READ_IOPS / $CMP_SEQ_WRITE_IOPS"
-	SUMMARY+=$cxt
+    local first_randread="${3}"
+    local first_cpu_idle_pct_randread="${4}"
+    local first_randwrite="${5}"
+    local first_cpu_idle_pct_randwrite="${6}"
+    local second_randread="${7}"
+    local second_cpu_idle_pct_randread="${8}"
+    local second_randwrite="${9}"
+    local second_cpu_idle_pct_randwrite="${10}"
+    local cmp_randread="${11}"
+    local cmp_cpu_idle_pct_randread="${12}"
+    local cmp_randwrite="${13}"
+    local cmp_cpu_idle_pct_randwrite="${14}"
 
-	printf -v cxt "Bandwidth in KiB/sec (Read/Write)\n$CMP_FMT$CMP_FMT\n" \
-		"Random:" \
-		"$(commaize $FIRST_RAND_READ_BW) / $(commaize $FIRST_RAND_WRITE_BW)" \
-		"vs" \
-		"$(commaize $SECOND_RAND_READ_BW) / $(commaize $SECOND_RAND_WRITE_BW)" \
-		":" \
-		"$CMP_RAND_READ_BW / $CMP_RAND_WRITE_BW" \
-		"Sequential:" \
-		"$(commaize $FIRST_SEQ_READ_BW) / $(commaize $FIRST_SEQ_WRITE_BW)" \
-		"vs" \
-		"$(commaize $SECOND_SEQ_READ_BW) / $(commaize $SECOND_SEQ_WRITE_BW)" \
-		":" \
-		"$CMP_SEQ_READ_BW / $CMP_SEQ_WRITE_BW"
-	SUMMARY+=$cxt
+    local first_seqread="${15}"
+    local first_cpu_idle_pct_seqread="${16}"
+    local first_seqwrite="${17}"
+    local first_cpu_idle_pct_seqwrite="${18}"
+    local second_seqread="${19}"
+    local second_cpu_idle_pct_seqread="${20}"
+    local second_seqwrite="${21}"
+    local second_cpu_idle_pct_seqwrite="${22}"
+    local cmp_seqread="${23}"
+    local cmp_cpu_idle_pct_seqread="${24}"
+    local cmp_seqwrite="${25}"
+    local cmp_cpu_idle_pct_seqwrite="${26}"
 
-	printf -v cxt "Latency in ns (Read/Write)\n$CMP_FMT$CMP_FMT\n" \
-		"Random:" \
-		"$(commaize $FIRST_RAND_READ_LAT) / $(commaize $FIRST_RAND_WRITE_LAT)" \
-		"vs" \
-		"$(commaize $SECOND_RAND_READ_LAT) / $(commaize $SECOND_RAND_WRITE_LAT)" \
-		":" \
-		"$CMP_RAND_READ_LAT / $CMP_RAND_WRITE_LAT" \
-		"Sequential:" \
-		"$(commaize $FIRST_SEQ_READ_LAT) / $(commaize $FIRST_SEQ_WRITE_LAT)" \
-		"vs" \
-		"$(commaize $SECOND_SEQ_READ_LAT) / $(commaize $SECOND_SEQ_WRITE_LAT)" \
-		":" \
-		"$CMP_SEQ_READ_LAT / $CMP_SEQ_WRITE_LAT"
-		SUMMARY+=$cxt
+    if [ "$CPU_IDLE_PROF" = "enabled" ]; then
+        printf -v cxt "${metric_name} in ${metric_unit} with CPU idleness in percent (Read/Write)\n${CMP_FMT}${CMP_FMT}${CMP_FMT}\n" \
+            "Random:" \
+            "$(commaize "${first_randread}") ($(commaize "${first_cpu_idle_pct_randread}")) / $(commaize "${first_randwrite}") ($(commaize "${first_cpu_idle_pct_randwrite}"))" \
+            "vs" \
+            "$(commaize "${second_randread}") ($(commaize "${second_cpu_idle_pct_randread}")) / $(commaize "${second_randwrite}") ($(commaize "${second_cpu_idle_pct_randwrite}"))" ":" \
+            "${cmp_randread} (${cmp_cpu_idle_pct_randread}) / ${cmp_randwrite} (${cmp_cpu_idle_pct_randwrite})" \
+            "Sequential:" \
+            "$(commaize "${first_seqread}") ($(commaize "${first_cpu_idle_pct_seqread}")) / $(commaize "${first_seqwrite}") ($(commaize "${first_cpu_idle_pct_seqwrite}"))" \
+            "vs" \
+            "$(commaize "${second_seqread}") ($(commaize "${second_cpu_idle_pct_seqread}")) / $(commaize "${second_seqwrite}") ($(commaize "${second_cpu_idle_pct_seqwrite}"))" ":" \
+            "${cmp_seqread} (${cmp_cpu_idle_pct_seqread}) / ${cmp_seqwrite} (${cmp_cpu_idle_pct_seqwrite})"
+    else
+        printf -v cxt "${metric_name} in KiB/sec (Read/Write)\n${CMP_FMT}${CMP_FMT}${CMP_FMT}\n" \
+            "Random:" \
+            "$(commaize "${first_randread}") / $(commaize "${first_randwrite}")" \
+            "vs" \
+            "$(commaize "${second_randread}") / $(commaize "${second_randwrite}")" ":" \
+            "${cmp_randread} / ${cmp_randwrite}" \
+            "Sequential:" \
+            "$(commaize "${first_seqread}") / $(commaize "${first_seqwrite}")" \
+            "vs" \
+            "$(commaize "${second_seqread}") / $(commaize "${second_seqwrite}")" ":" \
+            "${cmp_seqread} / ${cmp_seqwrite}"
+    fi
+    SUMMARY+="${cxt}"
+}
+
+# Check CPU_IDLE_PROF status
+CPU_IDLE_STATUS="disabled"
+if [ "x$CPU_IDLE_PROF" = "xenabled" ]; then
+    CPU_IDLE_STATUS="enabled"
 fi
+
+# Example usage
+add_metrics_to_summary "IOPS" "ops" \
+    "$FIRST_RANDREAD_IOPS" "$FIRST_CPU_IDLE_PCT_RANDREAD_IOPS" \
+    "$FIRST_RANDWRITE_IOPS" "$FIRST_CPU_IDLE_PCT_RANDWRITE_IOPS" \
+    "$SECOND_RANDREAD_IOPS" "$SECOND_CPU_IDLE_PCT_RANDREAD_IOPS" \
+    "$SECOND_RANDWRITE_IOPS" "$SECOND_CPU_IDLE_PCT_RANDWRITE_IOPS" \
+    "$CMP_RANDREAD_IOPS" "$CMP_CPU_IDLE_PCT_RANDREAD_IOPS" \
+    "$CMP_RANDWRITE_IOPS" "$CMP_CPU_IDLE_PCT_RANDWRITE_IOPS" \
+    "$FIRST_SEQREAD_IOPS" "$FIRST_CPU_IDLE_PCT_SEQREAD_IOPS" \
+    "$FIRST_SEQWRITE_IOPS" "$FIRST_CPU_IDLE_PCT_SEQWRITE_IOPS" \
+    "$SECOND_SEQREAD_IOPS" "$SECOND_CPU_IDLE_PCT_SEQREAD_IOPS" \
+    "$SECOND_SEQWRITE_IOPS" "$SECOND_CPU_IDLE_PCT_SEQWRITE_IOPS" \
+    "$CMP_SEQREAD_IOPS" "$CMP_CPU_IDLE_PCT_SEQREAD_IOPS" \
+    "$CMP_SEQWRITE_IOPS" "$CMP_CPU_IDLE_PCT_SEQWRITE_IOPS"
+
+add_metrics_to_summary "Bandwidth" "KiB/sec" \
+    "$FIRST_RANDREAD_BANDWIDTH" "$FIRST_CPU_IDLE_PCT_RANDREAD_BANDWIDTH" \
+    "$FIRST_RANDWRITE_BANDWIDTH" "$FIRST_CPU_IDLE_PCT_RANDWRITE_BANDWIDTH" \
+    "$SECOND_RANDREAD_BANDWIDTH" "$SECOND_CPU_IDLE_PCT_RANDREAD_BANDWIDTH" \
+    "$SECOND_RANDWRITE_BANDWIDTH" "$SECOND_CPU_IDLE_PCT_RANDWRITE_BANDWIDTH" \
+    "$CMP_RANDREAD_BANDWIDTH" "$CMP_CPU_IDLE_PCT_RANDREAD_BANDWIDTH" \
+    "$CMP_RANDWRITE_BANDWIDTH" "$CMP_CPU_IDLE_PCT_RANDWRITE_BANDWIDTH" \
+    "$FIRST_SEQREAD_BANDWIDTH" "$FIRST_CPU_IDLE_PCT_SEQREAD_BANDWIDTH" \
+    "$FIRST_SEQWRITE_BANDWIDTH" "$FIRST_CPU_IDLE_PCT_SEQWRITE_BANDWIDTH" \
+    "$SECOND_SEQREAD_BANDWIDTH" "$SECOND_CPU_IDLE_PCT_SEQREAD_BANDWIDTH" \
+    "$SECOND_SEQWRITE_BANDWIDTH" "$SECOND_CPU_IDLE_PCT_SEQWRITE_BANDWIDTH" \
+    "$CMP_SEQREAD_BANDWIDTH" "$CMP_CPU_IDLE_PCT_SEQREAD_BANDWIDTH" \
+    "$CMP_SEQWRITE_BANDWIDTH" "$CMP_CPU_IDLE_PCT_SEQWRITE_BANDWIDTH"
+
+add_metrics_to_summary "Latency" "ns" \
+    "$FIRST_RANDREAD_LATENCY" "$FIRST_CPU_IDLE_PCT_RANDREAD_LATENCY" \
+    "$FIRST_RANDWRITE_LATENCY" "$FIRST_CPU_IDLE_PCT_RANDWRITE_LATENCY" \
+    "$SECOND_RANDREAD_LATENCY" "$SECOND_CPU_IDLE_PCT_RANDREAD_LATENCY" \
+    "$SECOND_RANDWRITE_LATENCY" "$SECOND_CPU_IDLE_PCT_RANDWRITE_LATENCY" \
+    "$CMP_RANDREAD_LATENCY" "$CMP_CPU_IDLE_PCT_RANDREAD_LATENCY" \
+    "$CMP_RANDWRITE_LATENCY" "$CMP_CPU_IDLE_PCT_RANDWRITE_LATENCY" \
+    "$FIRST_SEQREAD_LATENCY" "$FIRST_CPU_IDLE_PCT_SEQREAD_LATENCY" \
+    "$FIRST_SEQWRITE_LATENCY" "$FIRST_CPU_IDLE_PCT_SEQWRITE_LATENCY" \
+    "$SECOND_SEQREAD_LATENCY" "$SECOND_CPU_IDLE_PCT_SEQREAD_LATENCY" \
+    "$SECOND_SEQWRITE_LATENCY" "$SECOND_CPU_IDLE_PCT_SEQWRITE_LATENCY" \
+    "$CMP_SEQREAD_LATENCY" "$CMP_CPU_IDLE_PCT_SEQREAD_LATENCY" \
+    "$CMP_SEQWRITE_LATENCY" "$CMP_CPU_IDLE_PCT_SEQWRITE_LATENCY"
+
 
 echo "$SUMMARY" > $RESULT
 cat $RESULT
+
+
--- fio/func.sh
+++ fio/func.sh
@@ -1,83 +1,168 @@
 #!/bin/bash
 
-parse_iops() {
-        local OUTPUT_IOPS=$1
-        RAND_READ_IOPS=`cat $OUTPUT_IOPS | jq '.jobs[0].read.iops_mean'| cut -f1 -d.`
-        RAND_WRITE_IOPS=`cat $OUTPUT_IOPS | jq '.jobs[1].write.iops_mean'| cut -f1 -d.`
-        SEQ_READ_IOPS=`cat $OUTPUT_IOPS | jq '.jobs[2].read.iops_mean'| cut -f1 -d.`
-        SEQ_WRITE_IOPS=`cat $OUTPUT_IOPS | jq '.jobs[3].write.iops_mean'| cut -f1 -d.`
-        CPU_IDLE_PCT_IOPS=`cat $OUTPUT_IOPS | jq '.cpu_idleness.system' | cut -f1 -d.`
+# IOPS
+parse_seqread_iops() {
+    local OUTPUT=${1}
+    SEQREAD_IOPS=`cat $OUTPUT | jq '.jobs[0].read.iops_mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_SEQREAD_IOPS=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
 }
 
-parse_bw() {
-        local OUTPUT_BW=$1
-        RAND_READ_BW=`cat $OUTPUT_BW | jq '.jobs[0].read.bw_mean'| cut -f1 -d.`
-        RAND_WRITE_BW=`cat $OUTPUT_BW | jq '.jobs[1].write.bw_mean'| cut -f1 -d.`
-        SEQ_READ_BW=`cat $OUTPUT_BW | jq '.jobs[2].read.bw_mean'| cut -f1 -d.`
-        SEQ_WRITE_BW=`cat $OUTPUT_BW | jq '.jobs[3].write.bw_mean'| cut -f1 -d.`
-        CPU_IDLE_PCT_BW=`cat $OUTPUT_BW| jq '.cpu_idleness.system' | cut -f1 -d.`
+parse_seqwrite_iops() {
+    local OUTPUT=${1}
+    SEQWRITE_IOPS=`cat $OUTPUT | jq '.jobs[0].write.iops_mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_SEQWRITE_IOPS=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
 }
 
-parse_lat() {
-        local OUTPUT_LAT=$1
-        RAND_READ_LAT=`cat $OUTPUT_LAT | jq '.jobs[0].read.lat_ns.mean'| cut -f1 -d.`
-        RAND_WRITE_LAT=`cat $OUTPUT_LAT | jq '.jobs[1].write.lat_ns.mean'| cut -f1 -d.`
-        SEQ_READ_LAT=`cat $OUTPUT_LAT | jq '.jobs[2].read.lat_ns.mean'| cut -f1 -d.`
-        SEQ_WRITE_LAT=`cat $OUTPUT_LAT | jq '.jobs[3].write.lat_ns.mean'| cut -f1 -d.`
-        CPU_IDLE_PCT_LAT=`cat $OUTPUT_LAT| jq '.cpu_idleness.system' | cut -f1 -d.`
+parse_randread_iops() {
+    local OUTPUT=${1}
+    RANDREAD_IOPS=`cat $OUTPUT | jq '.jobs[0].read.iops_mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_RANDREAD_IOPS=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
 }
 
-FMT="%15s%25s\n"
-CMP_FMT="%15s%25s%5s%25s%5s%20s\n"
+parse_randwrite_iops() {
+    local OUTPUT=${1}
+    RANDWRITE_IOPS=`cat $OUTPUT | jq '.jobs[0].write.iops_mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_RANDWRITE_IOPS=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
+}
+
+# Bandwidth
+parse_seqread_bandwidth() {
+    local OUTPUT=${1}
+    SEQREAD_BANDWIDTH=`cat $OUTPUT | jq '.jobs[0].read.bw_mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_SEQREAD_BANDWIDTH=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
+}
+
+parse_seqwrite_bandwidth() {
+    local OUTPUT=${1}
+    SEQWRITE_BANDWIDTH=`cat $OUTPUT | jq '.jobs[0].write.bw_mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_SEQWRITE_BANDWIDTH=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
+}
+
+parse_randread_bandwidth() {
+    local OUTPUT=${1}
+    RANDREAD_BANDWIDTH=`cat $OUTPUT | jq '.jobs[0].read.bw_mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_RANDREAD_BANDWIDTH=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
+}
+
+parse_randwrite_bandwidth() {
+    local OUTPUT=${1}
+    RANDWRITE_BANDWIDTH=`cat $OUTPUT | jq '.jobs[0].write.bw_mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_RANDWRITE_BANDWIDTH=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
+}
+
+# Latency
+parse_seqread_latency() {
+    local OUTPUT=${1}
+    SEQREAD_LATENCY=`cat $OUTPUT | jq '.jobs[0].read.lat_ns.mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_SEQREAD_LATENCY=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
+}
+
+parse_seqwrite_latency() {
+    local OUTPUT=${1}
+    SEQWRITE_LATENCY=`cat $OUTPUT | jq '.jobs[0].write.lat_ns.mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_SEQWRITE_LATENCY=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
+}
+
+parse_randread_latency() {
+    local OUTPUT=${1}
+    RANDREAD_LATENCY=`cat $OUTPUT | jq '.jobs[0].read.lat_ns.mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_RANDREAD_LATENCY=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
+}
+
+parse_randwrite_latency() {
+    local OUTPUT=${1}
+    RANDWRITE_LATENCY=`cat $OUTPUT | jq '.jobs[0].write.lat_ns.mean'| cut -f1 -d.`
+    CPU_IDLE_PCT_RANDWRITE_LATENCY=`cat $OUTPUT | jq '.cpu_idleness.system' | cut -f1 -d.`
+}
+
+
+FMT="%25s%25s\n"
+CMP_FMT="%20s%30s%10s%30s%10s%25s\n"
 
 commaize() {
-	echo $1 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'
+    echo $1 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta'
 }
 
 calc_cmp_iops() {
-	DELTA_RAND_READ_IOPS=$(($SECOND_RAND_READ_IOPS-$FIRST_RAND_READ_IOPS))
-	CMP_RAND_READ_IOPS=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_RAND_READ_IOPS*100/$FIRST_RAND_READ_IOPS}"`"%"
-	DELTA_RAND_WRITE_IOPS=$(($SECOND_RAND_WRITE_IOPS-$FIRST_RAND_WRITE_IOPS))
-	CMP_RAND_WRITE_IOPS=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_RAND_WRITE_IOPS*100/$FIRST_RAND_WRITE_IOPS}"`"%"
-	DELTA_SEQ_READ_IOPS=$(($SECOND_SEQ_READ_IOPS-$FIRST_SEQ_READ_IOPS))
-	CMP_SEQ_READ_IOPS=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_SEQ_READ_IOPS*100/$FIRST_SEQ_READ_IOPS}"`"%"
-	DELTA_SEQ_WRITE_IOPS=$(($SECOND_SEQ_WRITE_IOPS-$FIRST_SEQ_WRITE_IOPS))
-	CMP_SEQ_WRITE_IOPS=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_SEQ_WRITE_IOPS*100/$FIRST_SEQ_WRITE_IOPS}"`"%"
-	CMP_CPU_IDLE_PCT_IOPS=$(($SECOND_CPU_IDLE_PCT_IOPS - $FIRST_CPU_IDLE_PCT_IOPS))
+    DELTA_RANDREAD_IOPS=$((${SECOND_RANDREAD_IOPS:-0}-${FIRST_RANDREAD_IOPS:-0}))
+    CMP_RANDREAD_IOPS=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_RANDREAD_IOPS:-0}*100/${FIRST_RANDREAD_IOPS:-0}}"`"%"
+    DELTA_RANDWRITE_IOPS=$((${SECOND_RANDWRITE_IOPS:-0}-${FIRST_RANDWRITE_IOPS:-0}))
+    CMP_RANDWRITE_IOPS=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_RANDWRITE_IOPS:-0}*100/${FIRST_RANDWRITE_IOPS:-0}}"`"%"
+    DELTA_SEQREAD_IOPS=$((${SECOND_SEQREAD_IOPS:-0}-${FIRST_SEQREAD_IOPS:-0}))
+    CMP_SEQREAD_IOPS=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_SEQREAD_IOPS:-0}*100/${FIRST_SEQREAD_IOPS:-0}}"`"%"
+    DELTA_SEQWRITE_IOPS=$((${SECOND_SEQWRITE_IOPS:-0}-${FIRST_SEQWRITE_IOPS:-0}))
+    CMP_SEQWRITE_IOPS=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_SEQWRITE_IOPS:-0}*100/${FIRST_SEQWRITE_IOPS:-0}}"`"%"
+
+    DELTA_CPU_IDLE_PCT_RANDREAD_IOPS=$((${SECOND_CPU_IDLE_PCT_RANDREAD_IOPS:-0}-${FIRST_CPU_IDLE_PCT_RANDREAD_IOPS:-0}))
+    CMP_CPU_IDLE_PCT_RANDREAD_IOPS=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_RANDREAD_IOPS:-0}*100/${FIRST_CPU_IDLE_PCT_RANDREAD_IOPS:-0}}"`"%"
+    DELTA_CPU_IDLE_PCT_RANDWRITE_IOPS=$((${SECOND_CPU_IDLE_PCT_RANDWRITE_IOPS:-0}-${FIRST_CPU_IDLE_PCT_RANDWRITE_IOPS:-0}))
+    CMP_CPU_IDLE_PCT_RANDWRITE_IOPS=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_RANDWRITE_IOPS:-0}*100/${FIRST_CPU_IDLE_PCT_RANDWRITE_IOPS:-0}}"`"%"
+    DELTA_CPU_IDLE_PCT_SEQREAD_IOPS=$((${SECOND_CPU_IDLE_PCT_SEQREAD_IOPS:-0}-${FIRST_CPU_IDLE_PCT_SEQREAD_IOPS:-0}))
+    CMP_CPU_IDLE_PCT_SEQREAD_IOPS=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_SEQREAD_IOPS:-0}*100/${FIRST_CPU_IDLE_PCT_SEQREAD_IOPS:-0}}"`"%"
+    DELTA_CPU_IDLE_PCT_SEQWRITE_IOPS=$((${SECOND_CPU_IDLE_PCT_SEQWRITE_IOPS:-0}-${FIRST_CPU_IDLE_PCT_SEQWRITE_IOPS:-0}))
+    CMP_CPU_IDLE_PCT_SEQWRITE_IOPS=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_SEQWRITE_IOPS:-0}*100/${FIRST_CPU_IDLE_PCT_SEQWRITE_IOPS:-0}}"`"%"
 }
 
-calc_cmp_bw() {
-	DELTA_RAND_READ_BW=$(($SECOND_RAND_READ_BW-$FIRST_RAND_READ_BW))
-	CMP_RAND_READ_BW=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_RAND_READ_BW*100/$FIRST_RAND_READ_BW}"`"%"
-	DELTA_RAND_WRITE_BW=$(($SECOND_RAND_WRITE_BW-$FIRST_RAND_WRITE_BW))
-	CMP_RAND_WRITE_BW=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_RAND_WRITE_BW*100/$FIRST_RAND_WRITE_BW}"`"%"
-	DELTA_SEQ_READ_BW=$(($SECOND_SEQ_READ_BW-$FIRST_SEQ_READ_BW))
-	CMP_SEQ_READ_BW=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_SEQ_READ_BW*100/$FIRST_SEQ_READ_BW}"`"%"
-	DELTA_SEQ_WRITE_BW=$(($SECOND_SEQ_WRITE_BW-$FIRST_SEQ_WRITE_BW))
-	CMP_SEQ_WRITE_BW=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_SEQ_WRITE_BW*100/$FIRST_SEQ_WRITE_BW}"`"%"
-	CMP_CPU_IDLE_PCT_BW=$(($SECOND_CPU_IDLE_PCT_BW - $FIRST_CPU_IDLE_PCT_BW))
+calc_cmp_bandwidth() {
+    DELTA_RANDREAD_BANDWIDTH=$((${SECOND_RANDREAD_BANDWIDTH:-0}-${FIRST_RANDREAD_BANDWIDTH:-0}))
+    CMP_RANDREAD_BANDWIDTH=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_RANDREAD_BANDWIDTH:-0}*100/${FIRST_RANDREAD_BANDWIDTH:-0}}"`"%"
+    DELTA_RANDWRITE_BANDWIDTH=$((${SECOND_RANDWRITE_BANDWIDTH:-0}-${FIRST_RANDWRITE_BANDWIDTH:-0}))
+    CMP_RANDWRITE_BANDWIDTH=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_RANDWRITE_BANDWIDTH:-0}*100/${FIRST_RANDWRITE_BANDWIDTH:-0}}"`"%"
+    DELTA_SEQREAD_BANDWIDTH=$((${SECOND_SEQREAD_BANDWIDTH:-0}-${FIRST_SEQREAD_BANDWIDTH:-0}))
+    CMP_SEQREAD_BANDWIDTH=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_SEQREAD_BANDWIDTH:-0}*100/${FIRST_SEQREAD_BANDWIDTH:-0}}"`"%"
+    DELTA_SEQWRITE_BANDWIDTH=$((${SECOND_SEQWRITE_BANDWIDTH:-0}-${FIRST_SEQWRITE_BANDWIDTH:-0}))
+    CMP_SEQWRITE_BANDWIDTH=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_SEQWRITE_BANDWIDTH:-0}*100/${FIRST_SEQWRITE_BANDWIDTH:-0}}"`"%"
+
+    DELTA_CPU_IDLE_PCT_RANDREAD_BANDWIDTH=$((${SECOND_CPU_IDLE_PCT_RANDREAD_BANDWIDTH:-0}-${FIRST_CPU_IDLE_PCT_RANDREAD_BANDWIDTH:-0}))
+    CMP_CPU_IDLE_PCT_RANDREAD_BANDWIDTH=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_RANDREAD_BANDWIDTH:-0}*100/${FIRST_CPU_IDLE_PCT_RANDREAD_BANDWIDTH:-0}}"`"%"
+    DELTA_CPU_IDLE_PCT_RANDWRITE_BANDWIDTH=$((${SECOND_CPU_IDLE_PCT_RANDWRITE_BANDWIDTH:-0}-${FIRST_CPU_IDLE_PCT_RANDWRITE_BANDWIDTH:-0}))
+    CMP_CPU_IDLE_PCT_RANDWRITE_BANDWIDTH=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_RANDWRITE_BANDWIDTH:-0}*100/${FIRST_CPU_IDLE_PCT_RANDWRITE_BANDWIDTH:-0}}"`"%"
+    DELTA_CPU_IDLE_PCT_SEQREAD_BANDWIDTH=$((${SECOND_CPU_IDLE_PCT_SEQREAD_BANDWIDTH:-0}-${FIRST_CPU_IDLE_PCT_SEQREAD_BANDWIDTH:-0}))
+    CMP_CPU_IDLE_PCT_SEQREAD_BANDWIDTH=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_SEQREAD_BANDWIDTH:-0}*100/${FIRST_CPU_IDLE_PCT_SEQREAD_BANDWIDTH:-0}}"`"%"
+    DELTA_CPU_IDLE_PCT_SEQWRITE_BANDWIDTH=$((${SECOND_CPU_IDLE_PCT_SEQWRITE_BANDWIDTH:-0}-${FIRST_CPU_IDLE_PCT_SEQWRITE_BANDWIDTH:-0}))
+    CMP_CPU_IDLE_PCT_SEQWRITE_BANDWIDTH=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_SEQWRITE_BANDWIDTH:-0}*100/${FIRST_CPU_IDLE_PCT_SEQWRITE_BANDWIDTH:-0}}"`"%"
 }
 
-calc_cmp_lat() {
-	DELTA_RAND_READ_LAT=$(($SECOND_RAND_READ_LAT-$FIRST_RAND_READ_LAT))
-	CMP_RAND_READ_LAT=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_RAND_READ_LAT*100/$FIRST_RAND_READ_LAT}"`"%"
-	DELTA_RAND_WRITE_LAT=$(($SECOND_RAND_WRITE_LAT-$FIRST_RAND_WRITE_LAT))
-	CMP_RAND_WRITE_LAT=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_RAND_WRITE_LAT*100/$FIRST_RAND_WRITE_LAT}"`"%"
-	DELTA_SEQ_READ_LAT=$(($SECOND_SEQ_READ_LAT-$FIRST_SEQ_READ_LAT))
-	CMP_SEQ_READ_LAT=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_SEQ_READ_LAT*100/$FIRST_SEQ_READ_LAT}"`"%"
-	DELTA_SEQ_WRITE_LAT=$(($SECOND_SEQ_WRITE_LAT-$FIRST_SEQ_WRITE_LAT))
-	CMP_SEQ_WRITE_LAT=`awk "BEGIN {printf \"%.2f\",
-		$DELTA_SEQ_WRITE_LAT*100/$FIRST_SEQ_WRITE_LAT}"`"%"
-	CMP_CPU_IDLE_PCT_LAT=$(($SECOND_CPU_IDLE_PCT_LAT - $FIRST_CPU_IDLE_PCT_LAT))
+calc_cmp_latency() {
+    DELTA_RANDREAD_LATENCY=$((${SECOND_RANDREAD_LATENCY:-0}-${FIRST_RANDREAD_LATENCY:-0}))
+    CMP_RANDREAD_LATENCY=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_RANDREAD_LATENCY:-0}*100/${FIRST_RANDREAD_LATENCY:-0}}"`"%"
+    DELTA_RANDWRITE_LATENCY=$((${SECOND_RANDWRITE_LATENCY:-0}-${FIRST_RANDWRITE_LATENCY:-0}))
+    CMP_RANDWRITE_LATENCY=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_RANDWRITE_LATENCY:-0}*100/${FIRST_RANDWRITE_LATENCY:-0}}"`"%"
+    DELTA_SEQREAD_LATENCY=$((${SECOND_SEQREAD_LATENCY:-0}-${FIRST_SEQREAD_LATENCY:-0}))
+    CMP_SEQREAD_LATENCY=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_SEQREAD_LATENCY:-0}*100/${FIRST_SEQREAD_LATENCY:-0}}"`"%"
+    DELTA_SEQWRITE_LATENCY=$((${SECOND_SEQWRITE_LATENCY:-0}-${FIRST_SEQWRITE_LATENCY:-0}))
+    CMP_SEQWRITE_LATENCY=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_SEQWRITE_LATENCY:-0}*100/${FIRST_SEQWRITE_LATENCY:-0}}"`"%"
+
+    DELTA_CPU_IDLE_PCT_RANDREAD_LATENCY=$((${SECOND_CPU_IDLE_PCT_RANDREAD_LATENCY:-0}-${FIRST_CPU_IDLE_PCT_RANDREAD_LATENCY:-0}))
+    CMP_CPU_IDLE_PCT_RANDREAD_LATENCY=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_RANDREAD_LATENCY:-0}*100/${FIRST_CPU_IDLE_PCT_RANDREAD_LATENCY:-0}}"`"%"
+    DELTA_CPU_IDLE_PCT_RANDWRITE_LATENCY=$((${SECOND_CPU_IDLE_PCT_RANDWRITE_LATENCY:-0}-${FIRST_CPU_IDLE_PCT_RANDWRITE_LATENCY:-0}))
+    CMP_CPU_IDLE_PCT_RANDWRITE_LATENCY=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_RANDWRITE_LATENCY:-0}*100/${FIRST_CPU_IDLE_PCT_RANDWRITE_LATENCY:-0}}"`"%"
+    DELTA_CPU_IDLE_PCT_SEQREAD_LATENCY=$((${SECOND_CPU_IDLE_PCT_SEQREAD_LATENCY:-0}-${FIRST_CPU_IDLE_PCT_SEQREAD_LATENCY:-0}))
+    CMP_CPU_IDLE_PCT_SEQREAD_LATENCY=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_SEQREAD_LATENCY:-0}*100/${FIRST_CPU_IDLE_PCT_SEQREAD_LATENCY:-0}}"`"%"
+    DELTA_CPU_IDLE_PCT_SEQWRITE_LATENCY=$((${SECOND_CPU_IDLE_PCT_SEQWRITE_LATENCY:-0}-${FIRST_CPU_IDLE_PCT_SEQWRITE_LATENCY:-0}))
+    CMP_CPU_IDLE_PCT_SEQWRITE_LATENCY=`awk "BEGIN {printf \"%.2f\",
+        ${DELTA_CPU_IDLE_PCT_SEQWRITE_LATENCY:-0}*100/${FIRST_CPU_IDLE_PCT_SEQWRITE_LATENCY:-0}}"`"%"
 }
